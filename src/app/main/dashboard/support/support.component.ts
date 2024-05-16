17f35111153833d33bfc4ddb416446e8e07a4146
AnjaliBhayal <142789636+AnjaliBhayal@users.noreply.github.com>
2024-05-16 16:27:40 +0530

    add support

--- src/app/main/dashboard/support/support.component.ts
+++ src/app/main/dashboard/support/support.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support.component.scss']
 })
 export class SupportComponent implements OnInit {
+sellerData: any;
 
   constructor() { }
 
